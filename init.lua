abcbf043649e061987fe9cf137908aad06d17ec3
Christopher Jones <christopher.jones2@yum.com>
2024-12-20 17:13:53 +0000

    execute lua keybindings

--- init.lua
+++ init.lua
@@ -1,1 +1,24 @@
+print "Loading cjonesuk nvim config..." 
+
 require("custom.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Executing lua
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Auto Commands
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+	vim.highlight.on_yank()
+    end,
+})
+
+print "Finished loading nvim config!"
+
