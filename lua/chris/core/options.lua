8a10d704bdcfa862d401f4ccbc69586131d9beec
Christopher Jones <christopher.jones2@yum.com>
2025-01-09 17:06:56 +0000

    only set shell on windows

--- lua/chris/core/options.lua
+++ lua/chris/core/options.lua
@@ -3,6 +3,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
-if vim.fn.has("win32") then
+if vim.fn.has("win32") == 1 then
   vim.o.shell = "pwsh -NoLogo"
 end
