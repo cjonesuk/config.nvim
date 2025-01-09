6a62c475c028e4389d0fe805f7a50f7594b30a7e
Chris <chrisjones170187@gmail.com>
2025-01-09 21:50:00 +0000

    add key binding for go to source definition

--- lua/chris/plugins/typescript.lua
+++ lua/chris/plugins/typescript.lua
@@ -4,9 +4,12 @@ return {
     enabled = true,
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     config = function()
+      local keymap = vim.keymap
       local typescript = require("typescript-tools")
 
       typescript.setup({})
+
+      keymap.set("n", "grd", "<cmd>TSToolsGoToSourceDefinition<CR>", { desc = "Go to Source Definition" })
     end,
   },
 }
