95ea261e71f9c24a6af3909318ace8b5659d5543
Chris <chrisjones170187@gmail.com>
2025-01-13 14:25:57 +0000

    mini.ai
    Adds mini.ai for improved textobject support
    Binds F to custom textobject for function definition

--- lua/chris/plugins/mini.lua
+++ lua/chris/plugins/mini.lua
@@ -1,8 +1,18 @@
-return { 
-  'echasnovski/mini.nvim',
+return {
+  "echasnovski/mini.nvim",
   enabled = true,
   config = function()
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = true }
-  end
+    local statusline = require("mini.statusline")
+    local ai = require("mini.ai")
+    local gen_spec = ai.gen_spec
+
+    statusline.setup({ use_icons = true })
+
+    ai.setup({
+      custom_textobjects = {
+        -- Function definition (needs treesitter queries with these captures)
+        F = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+      },
+    })
+  end,
 }
