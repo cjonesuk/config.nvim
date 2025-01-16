e0ddec100d21928b45927e25f714f190a79c00e2
Christopher Jones <christopher.jones2@yum.com>
2025-01-16 15:29:33 +0000

    terminal title

--- lua/chris/core/options.lua
+++ lua/chris/core/options.lua
@@ -6,3 +6,6 @@ vim.opt.clipboard = "unnamedplus"
 if vim.fn.has("win32") == 1 then
   vim.o.shell = "pwsh -NoLogo"
 end
+
+vim.opt.title = true
+vim.opt.titlestring = [[%t - %{fnamemodify(getcwd(), ':t')}]]
