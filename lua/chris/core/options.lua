c8dc9bd8870918a68d9bf2e4495f338dc939dd32
Christopher Jones <christopher.jones2@yum.com>
2025-02-03 14:30:00 +0000

    disable relative line numbers

--- lua/chris/core/options.lua
+++ lua/chris/core/options.lua
@@ -1,6 +1,6 @@
 vim.opt.shiftwidth = 2
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.clipboard = "unnamedplus"
 
 if vim.fn.has("win32") == 1 then
--- lua/chris/plugins/numbertoggle.lua
+++ lua/chris/plugins/numbertoggle.lua
@@ -1,5 +1,6 @@
 return {
   {
     "jeffkreeftmeijer/vim-numbertoggle",
+    enabled = false,
   },
 }
