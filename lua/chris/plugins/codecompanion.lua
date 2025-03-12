73d7fc343e07dde0272d57c256f168be6a4ef9d4
Christopher Jones <christopher.jones2@yum.com>
2025-03-12 10:27:06 +0000

    install codecompanion

---
+++ lua/chris/plugins/codecompanion.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("codecompanion").setup({
+        strategies = {
+          chat = {
+            adapter = "copilot",
+          },
+          inline = {
+            adapter = "copilot",
+          },
+        },
+      })
+    end,
+  },
+}
