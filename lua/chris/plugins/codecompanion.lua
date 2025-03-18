87ada4a6224c00b5ba0f4edd015c9877527d87c8
Christopher Jones <christopher.jones2@yum.com>
2025-03-18 14:40:57 +0000

    expand codecompanion config

--- lua/chris/plugins/codecompanion.lua
+++ lua/chris/plugins/codecompanion.lua
@@ -6,17 +6,51 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-      require("codecompanion").setup({
-        strategies = {
-          chat = {
-            adapter = "copilot",
+    cmd = {
+      "CodeCompanion",
+      "CodeCompanionActions",
+      "CodeCompanionChat",
+      "CodeCompanionCmd",
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "copilot",
+          keymaps = {
+            send = {
+              mode = { n = "<C-s>", i = "<C-s>" },
+            },
+            close = {
+              mode = { n = "<C-c>", i = "<C-c>" },
+            },
           },
-          inline = {
-            adapter = "copilot",
+          slash_commands = {
+            ["file"] = {
+              callback = "strategies.chat.slash_commands.file",
+              description = "Select a file using fzf",
+              opts = {
+                provider = "snacks", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+                contains_code = true,
+              },
+            },
           },
         },
-      })
-    end,
+        inline = {
+          adapter = "copilot",
+        },
+      },
+    },
+    keys = {
+      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "AI Toggle [C]hat" },
+      { "<leader>an", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "AI [N]ew Chat" },
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "AI [A]ction" },
+      { "ga", "<cmd>CodeCompanionChat Add<CR>", mode = { "v" }, desc = "AI [A]dd to Chat" },
+      -- prompts
+      { "<leader>ae", "<cmd>CodeCompanion /explain<cr>", mode = { "v" }, desc = "AI [E]xplain" },
+    },
+    -- config = function()
+    --   -- Expand 'cc' into 'CodeCompanion' in the command line
+    --   vim.cmd([[cab cc CodeCompanion]])
+    -- end,
   },
 }
